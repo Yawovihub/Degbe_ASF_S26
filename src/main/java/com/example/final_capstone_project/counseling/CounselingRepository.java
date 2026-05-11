901f7ddb4d9f19839c69c5529ffdfa28720444bb
Yawovi Degbe <151958343+Yawovihub@users.noreply.github.com>
2026-05-11 08:52:14 -0500

    Delete src directory

--- src/main/java/com/example/final_capstone_project/counseling/CounselingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.final_capstone_project.counseling;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CounselingRepository extends JpaRepository<Counseling, Long> {
-
-}
--- src/utils/data.js
+++
@@ -1,6 +0,0 @@
-import {useState} from "react";
-
-const [users, setUsers] = useState([
-    { id: 1, name: 'John Doe', email: 'john@example.com', role: 'Administrator', status: 'Active', joined: 'Oct 24, 2025' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com', role: 'Editor', status: 'Inactive', joined: 'Nov 12, 2025' },
-]);
